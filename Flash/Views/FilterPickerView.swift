b9da64e5b91563e59cd6f8a6625ed5aca554330a
shreyaspatil <patil.shreyas@northeastern.edu>
2024-10-13 20:21:38 -0400

    updated structure

---
+++ Flash/Views/FilterPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterPickerView.swift
+//  Flash
+//
+//  Created by Shreyas Patil on 10/13/24.
+//
+
+import SwiftUI
+
+struct FilterPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterPickerView()
+}
